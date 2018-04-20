673386ad2f82bc4f7b1b26618df77ca734668596
AndreyBelonog <36847003+AndreyBelonog@users.noreply.github.com>
2018-04-20 10:40:26 +0300

    Initial Eclipse-testing commit

---
+++ TestClass/src/TestClass.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("I'll become a great developer!");
+	}
+}
