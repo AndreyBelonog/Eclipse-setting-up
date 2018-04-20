f7c659b91bb30c59a5e848eb85690b077cd33a68
AndreyBelonog <36847003+AndreyBelonog@users.noreply.github.com>
2018-04-20 10:51:08 +0300

    Refactored

--- TestClass/src/TestClass.java
+++ TestClass/src/TestClass.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("I'll become a great developer!");
+		System.out.println("I'll become a great developer!");		
 	}
 }
